351dd53f58aa4c129044c8c808887748fbb89ced
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-07 20:59:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [David, Alexis, Moira, Johnny]
+  ["David", "Alexis", "Moira", "Johnny"]
 end
 
 def add_element_to_end_of_array(array, element)
